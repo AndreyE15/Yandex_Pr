3ea9c50062f439fe5152e8cef6379d24c28a1245
Andrey Ermakov <bunakoff.andrei2014@yandex.ru>
2021-12-07 23:32:34 +0300

    Order

---
+++ bub_met/main_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a,b,c,tmp;
+  cin >> a >> b >> c;
+    if(a>b)
+   {tmp=a;a=b;b=tmp;}
+    if(a>c)
+   {tmp=a;a=c;c=tmp;}
+    if(b>c)
+   {tmp=b;b=c;c=tmp;}
+    cout <<a<<" "<<b<<" "<<c<< endl;
+    cout <<"One change"<< endl;
+  return 0;
+  } //упорядочили элементы методом пузырька
